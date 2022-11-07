3012fa3dd957d7042291424d65c7bf31e29d1243
Stirling-Silver <113132128+Stirling-Silver@users.noreply.github.com>
2022-11-07 11:17:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ let numPufferEl = document.getElementById("num-puffer");
 
 // Global Variables
 let character = "Steve";
+let numCod = 0;
+let numSalmon = 0;
+let numTropical = 0;
+let numPuffer = 0;
 
 // Event Listeners
 steveImgEl.addEventListener("click", selectSteve);
@@ -40,12 +44,45 @@ function fishOnce() {
     // Use Steve Probability Distribution for fishing
     let randNum = Math.random();
     console.log(randNum);
-    if (randNum < 0.7) console.log("Raw Cod");
-  } else if (randNum < 0.9) {
-    console.log("Raw Salmon");
-  } else if (randNum < 0.95) {
-    console.log("Tropical Fish");
+
+    if (randNum < 0.7) {
+      numCod++;
+      imgResultEl.src = "img/Raw-Cod.png";
+      numCodEl.innerHTML = numCod;
+    } else if (randNum < 0.9) {
+      numSalmon++;
+      imgResultEl.src = "img/Raw-Salmon.png";
+      numSalmonEl.innerHTML = numSalmon;
+    } else if (randNum < 0.95) {
+      numTropical++;
+      imgResultEl.src = "img/Tropical-Fish.png";
+      numTropicalEl.innerHTML = numTropical;
+    } else {
+      numPuffer++;
+      imgResultEl.src = "img/Pufferfish.png";
+      numPufferEl.innerHTML = numPuffer;
+    }
   } else {
-    console.log("Puffer Fish");
+    // Use Alex Propability Distribution for Fishing
+    let randNum = Math.random();
+    console.log(randNum);
+
+    if (randNum < 0.1) {
+      numCod++;
+      imgResultEl.src = "img/Raw-Cod.png";
+      numCodEl.innerHTML = numCod;
+    } else if (randNum < 0.2) {
+      numSalmon++;
+      imgResultEl.src = "img/Raw-Salmon.png";
+      numSalmonEl.innerHTML = numSalmon;
+    } else if (randNum < 0.5) {
+      numTropical++;
+      imgResultEl.src = "img/Tropical-Fish.png";
+      numTropicalEl.innerHTML = numTropical;
+    } else {
+      numPuffer++;
+      imgResultEl.src = "img/Pufferfish.png";
+      numPufferEl.innerHTML = numPuffer;
+    }
   }
 }
